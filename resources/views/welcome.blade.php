2496461075521977ad1b6f2e64ec39f17250476a
Shakil Alam <itxshakiil@gmail.com>
2020-03-13 10:42:20 +0530

    Added Shop Link

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,7 +21,7 @@
               </a>
             </div>
             <div class="mt-3 sm:mt-0 sm:ml-3">
-              <a href="#" class="w-full flex items-center justify-center px-8 py-3 text-xs  border border-transparent text-base leading-6 font-medium rounded-lg text-indigo-700 bg-indigo-100 hover:text-indigo-600 hover:bg-indigo-50 focus:outline-none focus:shadow-outline focus:border-indigo-300 transition duration-150 ease-in-out md:py-4 md:px-10">
+              <a href="/shop" class="w-full flex items-center justify-center px-8 py-3 text-xs  border border-transparent text-base leading-6 font-medium rounded-lg text-indigo-700 bg-indigo-100 hover:text-indigo-600 hover:bg-indigo-50 focus:outline-none focus:shadow-outline focus:border-indigo-300 transition duration-150 ease-in-out md:py-4 md:px-10">
                 Shop by category
               </a>
             </div>
