ae55e6cfeb3c50edd499ed51ad40ba5648d365fb
Shakil Alam <itxshakil@gmail.com>
2020-07-27 13:33:16 +0530

    new laravel 7 syntax

--- database/migrations/2020_03_09_092912_create_category_product_pivot_tables.php
+++ database/migrations/2020_03_09_092912_create_category_product_pivot_tables.php
@@ -16,13 +16,8 @@ class CreateCategoryProductPivotTables extends Migration
         Schema::create('category_product', function (Blueprint $table) {
             $table->bigIncrements('id');
 
-            $table->unsignedBigInteger('product_id');
-            $table->foreign('product_id')->references('id')
-            ->on('products')->onDelete('cascade');
-
-            $table->unsignedBigInteger('category_id');
-            $table->foreign('category_id')->references('id')
-            ->on('categories')->onDelete('cascade');
+            $table->foreignId('product_id')->constrained();
+            $table->foreignId('category_id')->constrained();
 
             $table->timestamps();
         });
