a790818db38d6f16534ecb1e5a3f25fead3e67c8
Shakil Alam <itxshakil@gmail.com>
2021-02-07 20:39:13 +0530

    Some more fixes

--- resources/views/products/view.blade.php
+++ resources/views/products/view.blade.php
@@ -7,9 +7,9 @@
 @endsection
 @section('content')
 <div class="container mx-auto px-6 my-4">
-    <form action="/search" method="get" class="w-full mb-4 text-right mr-4">
+    <form action="{{route('search')}}" method="GET" class="w-full mb-4 text-right mr-4">
         <input
-            class="w-48 px-3 py-2 text-sm leading-tight text-gray-700 border  rounded shadow appearance-none focus:outline-none @error('q')der-red-500 @enderror"
+            class="w-48 px-3 py-2 text-sm leading-tight text-gray-700 border  rounded shadow appearance-none focus:outline-none @error('q')border-red-500 @enderror"
             id="query" type="search" name="q" placeholder="Search products" />
         @error('q')
         <p class="text-xs italic text-red-500" role="alert">{{ $message }}</p>
