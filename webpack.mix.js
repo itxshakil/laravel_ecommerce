d46a07ab2c3d15c7470b60f0b663fa847606a373
Shakil Alam <itxshakil@gmail.com>
2021-09-26 22:04:44 +0530

    Update laravel-mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,8 +14,8 @@ const tailwindcss = require('tailwindcss')
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-  .sass('resources/sass/app.scss', 'public/css')
+mix.js('resources/js/app.js', 'public/js').vue();
+mix.sass('resources/sass/app.scss', 'public/css')
   .options({
     processCssUrls: false,
     postCss: [tailwindcss('./tailwind.config.js')],
